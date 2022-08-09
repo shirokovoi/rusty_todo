dd6f78744044d4537d87e96be2e8eaccc49cfdce
Oleg Shirokov <oleeq2@gmail.com>
2022-08-09 23:51:50 +0300

    Initial migration

--- migrations/20220809164619_init.sql
+++ migrations/20220809164619_init.sql
@@ -1,1 +1,9 @@
 -- Add migration script here
+CREATE TABLE Users(Id int not null primary key, Username int not null , HashedPassword int not null);
+CREATE INDEX UsersUsernameIdx on Users(Username);
+
+CREATE TABLE TodoList(Id int not null primary key, OwnerId int not null);
+ALTER TABLE TodoListOwner_fk ADD FOREIGN KEY (OwnerId) REFERENCES Users(Id);
+
+CREATE TABLE TodoEntry(Id int not null primary key, Priority int not null, ListId int not null, Value varchar(2048));
+ALTER TABLE TodoEntryListId_fk ADD FOREIGN KEY (ListId) REFERENCES TodoList(Id);
