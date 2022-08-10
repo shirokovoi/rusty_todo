d76b7187f42160b95b47c35db4f0292417967c7a
Oleg Shirokov <oleeq2@gmail.com>
2022-08-11 00:51:00 +0300

    Add version into transaction

--- migrations/20220809164619_init.sql
+++ migrations/20220809164619_init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Users(Id int not null primary key, Username int not null , HashedPassword int not null);
 CREATE INDEX UsersUsernameIdx on Users(Username);
 
-CREATE TABLE TodoList(Id int not null primary key, OwnerId int not null);
+CREATE TABLE TodoList(Id int not null primary key, OwnerId int not null, Version int not null default 0);
 ALTER TABLE TodoList ADD CONSTRAINT TodoListOwner_fk FOREIGN KEY (OwnerId) REFERENCES Users(Id);
 
 CREATE TABLE TodoEntry(Id int not null primary key, Priority int not null, ListId int not null, Value varchar(2048));
