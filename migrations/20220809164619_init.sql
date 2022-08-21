7877c97a61d273cc3f45a2dd7fa99bbec90a646c
Oleg Shirokov <oleeq2@gmail.com>
2022-08-22 00:57:36 +0300

    Fix typo in migration

--- migrations/20220809164619_init.sql
+++ migrations/20220809164619_init.sql
@@ -2,7 +2,7 @@
 CREATE TABLE Users(id serial not null primary key, username varchar(100) not null, hashed_password varchar(72) not null, CONSTRAINT unique_username UNIQUE (username));
 CREATE INDEX UsersUsernameIdx on Users(username);
 
-CREATE TABLE TodoList(id serial not null primary key, owner_id int not null, version int not null default 0 CONSTRAINT unique_owner UNIQUE(owner_id));
+CREATE TABLE TodoList(id serial not null primary key, owner_id int not null, version int not null default 0, CONSTRAINT unique_owner UNIQUE(owner_id));
 ALTER TABLE TodoList ADD CONSTRAINT TodoListOwner_fk FOREIGN KEY (owner_id) REFERENCES Users(id);
 
 CREATE TABLE TodoEntry(id serial not null primary key, priority int not null, list_id int not null, value varchar(2048) not null);
